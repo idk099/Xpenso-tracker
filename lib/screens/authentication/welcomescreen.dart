b3f53349888db7428a3b87726f7d4755a01fc840
Sreehari <psreehari666@gmail.com>
2024-04-22 14:12:55 +0530

    first

---
+++ lib/screens/authentication/welcomescreen.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xpenso/screens/authentication/signup.dart';
+import 'package:xpenso/screens/authentication/signin.dart';
+
+import 'package:xpenso/services/authenticate.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+ 
+  Image? background;
+  final Authenticate _auth = Authenticate();
+   bool _loading = false;
+  @override
+  void initState() {
+    super.initState();
+    background = Image.asset('assets/images/3.jpg');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(background!.image, context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: background!.image,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/X.png',
+              height: 220,
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: _submit,
+                        child:  Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: _loading?SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: CircularProgressIndicator(
+                                          color: Colors.blue),
+                                    ):const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image:
+                                    AssetImage('assets/images/google_logo.png'),
+                                height: 35,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                "CONTINUE WITH GOOGLE",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      height: 46,
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Signin(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "SIGN IN WITH EMAIL",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      height: 46,
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Signup(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "SIGN UP WITH EMAIL",
+                          style: const TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submit() async {
+    
+    setState(() {
+      _loading = true;
+    });
+    try {
+      await _auth.gsignin();
+    } catch (e) {
+      setState(() {
+        _loading = false;
+      });
+      print(e);
+      if (e.toString() != 'Null check operator used on a null value') {
+        Get.snackbar(
+          'SOMETHING WENT WRONG',
+          'Try again',
+          colorText: Colors.white,
+          duration: const Duration(seconds: 1),
+        );
+      }
+    }
+  }
+}
